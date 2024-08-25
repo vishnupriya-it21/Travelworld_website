89fd9018526a9f97dda0c5444b28a8c0e59622e1
vishnupriya-it21 <vishnupriya.it21@bitsathy.ac.in>
2024-08-25 11:45:07 +0530

    Remove unnecessary files, keep only frontend folders

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-import Layout from "F:/MernStack-Tour-Management-main/tour-management/frontend/src/components/Layout/Layout";
-
-function App() {
-  return <Layout/>;
-}
-
-export default App;
